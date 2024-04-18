7c2413a2868c7c069e7d9cbc59f63c3b90e833d3
Rishi2804 <77213736+Rishi2804@users.noreply.github.com>
2024-04-18 12:33:12 -0400

    feat: reworked graphql fetch query

--- app/index.js
+++ app/index.js
@@ -8,74 +8,99 @@ import { useDexContext } from "../components/hooks/useDexContext"
 export default function Page() {
     const  { dex, dispatch } = useDexContext()
     useEffect(() => {
+
         const fetchPokemon = async () => {
-            console.log("Start fetching");
+            console.log("Start Fetching Pokemon")
             const query = `
-            query ($dex: String) {
-            dex: pokemon_v2_pokedex(where: {name: {_eq: $dex}}) {
-                dexName: name
-                results: pokemon_v2_pokemondexnumbers(order_by: {pokedex_number: asc}) {
-                entry: pokemon_v2_pokemonspecy {
+            query {
+                species: pokemon_v2_pokemonspecies(order_by: {id: asc}) {
                     name
-                    info: pokemon_v2_pokemons {
-                    types: pokemon_v2_pokemontypes {
-                        type: pokemon_v2_type {
-                            name
+                    varieties: pokemon_v2_pokemons {
+                        name
+                        is_default
+                        types: pokemon_v2_pokemontypes {
+                            type: pokemon_v2_type {
+                                name
+                            }
+                        }
+                        sprites: pokemon_v2_pokemonsprites {
+                            sprites(path: "other")
                         }
                     }
-                    sprites: pokemon_v2_pokemonsprites {
-                        imgPaths: sprites
-                    }
+                    dexNumbers: pokemon_v2_pokemondexnumbers {
+                        number: pokedex_number
+                        dex: pokemon_v2_pokedex {
+                            name
+                            region: pokemon_v2_region {
+                                name
+                            }
+                        }
                     }
                 }
-                dexNum: pokedex_number
-                }
-            }
             }              
             `
             const response = await fetch("https://beta.pokeapi.co/graphql/v1beta",
                 {
                     method: "POST",
                     body: JSON.stringify({
-                    query: query,
-                    variables: {dex: "national"}
+                        query: query
                     })
                 }
             )
 
             const json = await response.json()
+
             if (response.ok) {
-                let namesForFetch = []
-                let finalArray = json.data.dex[0].results.map((result) => {
-                    const homeImage = result.entry.info[0].sprites[0].imgPaths.other.home.front_default
-                    const tempImage = result.entry.info[0].sprites[0].imgPaths.other['official-artwork'].front_default
-                    if (!homeImage) namesForFetch.push(result.entry.name)
+                let picturesForFetch = []
+                let finalArray = json.data.species.map((species, index) => {
+                    const defaultForm = species.varieties.find((form) => form.is_default)
+                    const altForms = species.varieties.filter((form) => !form.is_default)
+                    const dexNumbers = species.dexNumbers.map((dex) => {
+                        return {
+                            number: dex.number,
+                            name: dex.dex.name,
+                            region: dex.dex.region ? dex.dex.region.name : dex.dex.region
+                        }
+                    })
+                    const nationalDexNumber = dexNumbers.find((dex) => dex.name === 'national').number
+                    const altDexNumbers = dexNumbers.filter((dex) => dex.name !== 'national')
+                    const homeImg = defaultForm.sprites[0].sprites.home.front_default
+                    const officialArtwork = defaultForm.sprites[0].sprites['official-artwork'].front_default
+                    if (!homeImg) picturesForFetch.push({name: defaultForm.name, formIndex: -1, dexIndex: index})
                     return {
-                        name: result.entry.name,
-                        dexNumber: result.dexNum,
-                        types: result.entry.info[0].types.map(type => type.type.name),
-                        image: homeImage ? homeImage : tempImage
+                        name: species.name,
+                        dexNumber: nationalDexNumber,
+                        types: defaultForm.types.map((item) => item.type.name),
+                        image: homeImg ? homeImg : officialArtwork,
+                        regionalDexNumbers: altDexNumbers,
+                        altForms: altForms.map((form, formIndex) => {
+                            const homeImg = form.sprites[0].sprites.home.front_default
+                            const officialArtwork = form.sprites[0].sprites['official-artwork'].front_default
+                            if (!homeImg) picturesForFetch.push({name: form.name, formIndex: formIndex, dexIndex: index})
+                            return {
+                                name: form.name,
+                                types: form.types.map((item) => item.type.name),
+                                image: homeImg ? homeImg : officialArtwork,
+                            }
+                        })
                     }
                 })
-
                 dispatch({type: 'SET_DEX_INFO', payload: finalArray})
-                console.log("Done fetching");
-
-                for (const name of namesForFetch) {
-                    const altResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+                console.log("Done fetching Pokemon");
+                for (const item of picturesForFetch) {
+                    const altResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${item.name}`);
                     const altJson = await altResponse.json();
                     const altImage = altJson.sprites.other.home.front_default
-
-                    for (let i = 0; i < finalArray.length; i++) {
-                        if (finalArray[i].name === name) {
-                            finalArray[i].image = altImage;
-                            break; // Stop searching once the name is found
+                    if (altResponse.ok) {
+                        if (item.formIndex === -1) {
+                            finalArray[item.dexIndex].image = altImage
+                        } else {
+                            finalArray[item.dexIndex].altForms[item.formIndex].image = altImage
                         }
                     }
                 }
-
                 dispatch({type: 'SET_DEX_INFO', payload: finalArray})
-                console.log("Done updating images");
+                console.log("Done fetching Missing Images");
             } else {
                 fetchPokemonAlt()
             }
