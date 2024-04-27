b84a70a60e557df5c818ec80899a3a8a69b2248d
Rishi2804 <77213736+Rishi2804@users.noreply.github.com>
2024-04-27 19:00:00 -0400

    feat: wrapped move modal in flatlist

--- components/MovesModal.js
+++ components/MovesModal.js
@@ -31,6 +31,109 @@ import LoadingView from "./LoadingView";
         if (pokemonLearnable.length === 0) fetchPokemon()
     }, [])
 
+    const MoveDetails = () => {
+        return (
+            <>
+                <View style={{flexDirection: "row", justifyContent: "center", paddingVertical: 20}}>
+                    <View style={styles.centeredHeader}>
+                        <Text style={styles.headerText}>power</Text>
+                        <Text style={styles.valueText}>{move.power ? move.power : "--"}</Text>
+                    </View>
+                    <View style={[styles.lineVert, {backgroundColor: typeToColourMap[move.type]}]} />
+                    <View style={styles.centeredHeader}>
+                        <Text style={styles.headerText}>accuracy</Text>
+                        <Text style={styles.valueText}>{move.accuracy ? move.accuracy + "%" : "--"}</Text>
+                    </View>
+                    <View style={[styles.lineVert, {backgroundColor: typeToColourMap[move.type]}]} />
+                    <View style={styles.centeredHeader}>
+                        <Text style={styles.headerText}>pp</Text>
+                        <Text style={styles.valueText}>{move.pp}</Text>
+                    </View>
+                </View>
+                <View style={{flexDirection: "row", marginBottom: 20}}>
+                    <View style={[styles.iconContainer, {backgroundColor: darkenColor(gradientMap[move.type], 0.25)}]}>
+                        <View style={{backgroundColor: typeToColourMap[move.type], height: 35, width: 35, alignItems: "center", justifyContent: "center", borderRadius: 5, marginBottom: 10}}>
+                            <IconTypeMapper type={move.type} width={35} height={35} fill={"white"}/>
+                        </View>
+                        <IconMoveClassMapper moveClass={move.class} width={35} height={35} />
+                    </View>
+                    <View style={{marginRight: 15, justifyContent: "center", flex: 1}}>
+                        <Text style={styles.descriptionText}>{displayDescription ? displayDescription.entry : move.descriptions[move.descriptions.length - 1]?.entry}</Text>
+                    </View>
+                </View>
+                <View style={[styles.line, {backgroundColor: typeToColourMap[move.type]}]}/>
+                {
+                    move.pastValues.length > 0 &&
+                    <>
+                    <View style={styles.section}>
+                        <Text style={styles.sectionHeaderText}>Past Values</Text>
+                        <View style={{backgroundColor: darkenColor(gradientMap[move.type], 0.25), borderRadius: 8, paddingVertical: 8}}>
+                            {
+                                move.pastValues.map((details, index) => {
+                                    const gamesAbr = details.games?.map(game => versionGroupAbr[game]).join(",")
+                                    return (
+                                        <View style={{flexDirection: "row", justifyContent: "center", marginBottom: 10, paddingBottom: index !== move.pastValues.length - 1 ? 15 : 0, flexWrap: "wrap", borderBottomWidth: index !== move.pastValues.length - 1 ? 1 : 0, borderBottomColor: typeToColourMap[move.type]}} key={index}>
+                                            <View style={{width: 150, justifyContent: "center"}}>
+                                                <Text style={styles.gameListText}>{gamesAbr}</Text>
+                                            </View>
+                                            <View>
+                                                <View style={{flexDirection: "row", justifyContent: "center"}}>
+                                                    <View style={styles.centered}>
+                                                        <Text style={styles.headerText2}>power</Text>
+                                                        <Text style={styles.valueText2}>{details.power ? details.power : "--"}</Text>
+                                                    </View>
+                                                    <View style={styles.centered}>
+                                                        <Text style={styles.headerText2}>accuracy</Text>
+                                                        <Text style={styles.valueText2}>{details.accuracy ? details.accuracy + "%" : "--"}</Text>
+                                                    </View>
+                                                    <View style={styles.centered}>
+                                                        <Text style={styles.headerText2}>pp</Text>
+                                                        <Text style={styles.valueText2}>{details.pp ? details.pp : "--"}</Text>
+                                                    </View>
+                                                </View>
+                                                {
+                                                    details.type && 
+                                                    <View style={{alignItems: "center"}}>
+                                                        <Text style={[styles.descriptionText2, {color: typeToColourMap[details.type]}]}>{"Type: " + formatText(details.type)}</Text>
+                                                    </View>
+                                                }
+                                            </View>
+                                        </View>
+                                    )
+                                })
+                            }
+                        </View>
+                    </View>
+                    <View style={[styles.line, {backgroundColor: typeToColourMap[move.type]}]}/>
+                    </>
+                }
+                <View style={styles.section}>
+                    <Text style={styles.sectionHeaderText}>Game Descriptions</Text>
+                    <View style={{backgroundColor: darkenColor(gradientMap[move.type], 0.25), borderRadius: 8, paddingVertical: 8, paddingHorizontal: 5}}>
+                        {
+                            move.descriptions.map((details, index) => {
+                                const gamesAbr = details.versionGroup.map(version => versionGroupAbr[version]).join(",")
+                                return (
+                                    <View style={{flexDirection: "row", marginBottom: 10, paddingBottom: index !== move.descriptions.length - 1 ? 15 : 0, borderBottomWidth: index !== move.descriptions.length - 1 ? 1 : 0, borderBottomColor: typeToColourMap[move.type]}} key={index}>
+                                        <View style={{width: 120, justifyContent: "center"}}>
+                                            <Text style={styles.gameListText}>{gamesAbr}</Text>
+                                        </View>
+                                        <View style={{justifyContent: "center", paddingLeft: 5, flex: 1}}>
+                                            <Text style={styles.descriptionText2}>{details.entry}</Text>
+                                        </View>
+                                    </View>
+                                )
+                            })
+                        }
+                    </View>
+                </View>
+                <View style={[styles.line, {backgroundColor: typeToColourMap[move.type]}]}/>
+                <View style={styles.section}>
+                    <Text style={styles.sectionHeaderText}>Learnable by</Text>
+                </View>
+            </>
+        )
+    }
     return (
         <View>
             <Pressable onPress={() => setIsVisible(true)}>
@@ -54,118 +157,27 @@ import LoadingView from "./LoadingView";
                     <View style={[styles.header, {backgroundColor: darkenColor(gradientMap[move.type], 0.25)}]}>
                         <Text style={styles.titleText}>{formatText(move.name)}</Text>
                     </View>
-                    <ScrollView>
-                        <View style={{flexDirection: "row", justifyContent: "center", paddingVertical: 20}}>
-                            <View style={styles.centeredHeader}>
-                                <Text style={styles.headerText}>power</Text>
-                                <Text style={styles.valueText}>{move.power ? move.power : "--"}</Text>
-                            </View>
-                            <View style={[styles.lineVert, {backgroundColor: typeToColourMap[move.type]}]} />
-                            <View style={styles.centeredHeader}>
-                                <Text style={styles.headerText}>accuracy</Text>
-                                <Text style={styles.valueText}>{move.accuracy ? move.accuracy + "%" : "--"}</Text>
-                            </View>
-                            <View style={[styles.lineVert, {backgroundColor: typeToColourMap[move.type]}]} />
-                            <View style={styles.centeredHeader}>
-                                <Text style={styles.headerText}>pp</Text>
-                                <Text style={styles.valueText}>{move.pp}</Text>
-                            </View>
-                        </View>
-                        <View style={{flexDirection: "row", marginBottom: 20}}>
-                            <View style={[styles.iconContainer, {backgroundColor: darkenColor(gradientMap[move.type], 0.25)}]}>
-                                <View style={{backgroundColor: typeToColourMap[move.type], height: 35, width: 35, alignItems: "center", justifyContent: "center", borderRadius: 5, marginBottom: 10}}>
-                                    <IconTypeMapper type={move.type} width={35} height={35} fill={"white"}/>
-                                </View>
-                                <IconMoveClassMapper moveClass={move.class} width={35} height={35} />
-                            </View>
-                            <View style={{marginRight: 15, justifyContent: "center", flex: 1}}>
-                                <Text style={styles.descriptionText}>{displayDescription ? displayDescription.entry : move.descriptions[move.descriptions.length - 1]?.entry}</Text>
-                            </View>
-                        </View>
-                        <View style={[styles.line, {backgroundColor: typeToColourMap[move.type]}]}/>
-                        {
-                            move.pastValues.length > 0 &&
-                            <>
-                            <View style={styles.section}>
-                                <Text style={styles.sectionHeaderText}>Past Values</Text>
-                                <View style={{backgroundColor: darkenColor(gradientMap[move.type], 0.25), borderRadius: 8, paddingVertical: 8}}>
-                                    {
-                                        move.pastValues.map((details, index) => {
-                                            const gamesAbr = details.games?.map(game => versionGroupAbr[game]).join(",")
-                                            return (
-                                                <View style={{flexDirection: "row", justifyContent: "center", marginBottom: 10, paddingBottom: index !== move.pastValues.length - 1 ? 15 : 0, flexWrap: "wrap", borderBottomWidth: index !== move.pastValues.length - 1 ? 1 : 0, borderBottomColor: typeToColourMap[move.type]}} key={index}>
-                                                    <View style={{width: 150, justifyContent: "center"}}>
-                                                        <Text style={styles.gameListText}>{gamesAbr}</Text>
-                                                    </View>
-                                                    <View>
-                                                        <View style={{flexDirection: "row", justifyContent: "center"}}>
-                                                            <View style={styles.centered}>
-                                                                <Text style={styles.headerText2}>power</Text>
-                                                                <Text style={styles.valueText2}>{details.power ? details.power : "--"}</Text>
-                                                            </View>
-                                                            <View style={styles.centered}>
-                                                                <Text style={styles.headerText2}>accuracy</Text>
-                                                                <Text style={styles.valueText2}>{details.accuracy ? details.accuracy + "%" : "--"}</Text>
-                                                            </View>
-                                                            <View style={styles.centered}>
-                                                                <Text style={styles.headerText2}>pp</Text>
-                                                                <Text style={styles.valueText2}>{details.pp ? details.pp : "--"}</Text>
-                                                            </View>
-                                                        </View>
-                                                        {
-                                                            details.type && 
-                                                            <View style={{alignItems: "center"}}>
-                                                                <Text style={[styles.descriptionText2, {color: typeToColourMap[details.type]}]}>{"Type: " + formatText(details.type)}</Text>
-                                                            </View>
-                                                        }
-                                                    </View>
-                                                </View>
-                                            )
-                                        })
-                                    }
-                                </View>
-                            </View>
-                            <View style={[styles.line, {backgroundColor: typeToColourMap[move.type]}]}/>
-                            </>
-                        }
-                        <View style={styles.section}>
-                            <Text style={styles.sectionHeaderText}>Game Descriptions</Text>
-                            <View style={{backgroundColor: darkenColor(gradientMap[move.type], 0.25), borderRadius: 8, paddingVertical: 8, paddingHorizontal: 5}}>
-                                {
-                                    move.descriptions.map((details, index) => {
-                                        const gamesAbr = details.versionGroup.map(version => versionGroupAbr[version]).join(",")
-                                        return (
-                                            <View style={{flexDirection: "row", marginBottom: 10, paddingBottom: index !== move.descriptions.length - 1 ? 15 : 0, borderBottomWidth: index !== move.descriptions.length - 1 ? 1 : 0, borderBottomColor: typeToColourMap[move.type]}} key={index}>
-                                                <View style={{width: 120, justifyContent: "center"}}>
-                                                    <Text style={styles.gameListText}>{gamesAbr}</Text>
-                                                </View>
-                                                <View style={{justifyContent: "center", paddingLeft: 5, flex: 1}}>
-                                                    <Text style={styles.descriptionText2}>{details.entry}</Text>
-                                                </View>
-                                            </View>
-                                        )
-                                    })
-                                }
-                            </View>
-                        </View>
-                        <View style={[styles.line, {backgroundColor: typeToColourMap[move.type]}]}/>
-                        <View style={styles.section}>
-                            <Text style={styles.sectionHeaderText}>Learnable by</Text>
-                            {
-                                loading && <LoadingView />
-                            }
-                            {
-                                !loading &&
-                                pokemonLearnable.map((mon, index) => {
-                                    return (
-                                        <View style={{marginBottom: 5}}>
-                                            <PokemonListView pokemon={mon} key={index} />
+                    {
+                        loading && <LoadingView />
+                    }
+                    {
+                        !loading &&
+                        <View>
+                            <FlatList 
+                                ListHeaderComponent={() => <MoveDetails />}
+                                data={pokemonLearnable}
+                                renderItem={({ item }) => {
+                                    return(
+                                        <View style={{marginHorizontal: 10}}>
+                                            <PokemonListView pokemon={item} key={item.id} />
                                         </View>
                                     )
-                                })
-                            }
+                                }}
+                                ItemSeparatorComponent={<View style={{height: 5}}/>}
+                                keyExtractor={(item) => item.id}
+                            />
                         </View>
-                    </ScrollView>
+                            }
                 </LinearGradient>
             </Modal>
         </View>        
