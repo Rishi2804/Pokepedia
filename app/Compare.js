409d20c95a02b0256143c66a3bb65e4a17444b76
Rishi2804 <77213736+Rishi2804@users.noreply.github.com>
2024-05-07 01:20:06 -0400

    feat: dark theme added to h2h

--- app/Compare.js
+++ app/Compare.js
@@ -1,26 +1,57 @@
 import { View, Text, SafeAreaView, StyleSheet, ScrollView, Image, TouchableOpacity } from 'react-native'
 import Dropdown from 'react-native-input-select';
 import { useDexContext } from '../components/hooks/useDexContext';
+import { useThemeContext } from '../components/hooks/useThemeContext';
 import { useState } from 'react';
 import { formatName, formatText, getTypeMatchups } from '../global/UtiliyFunctions';
 import { typeToColourMap } from '../maps/typeToColourMap';
 import IconTypeMapper from '../maps/typeToIconMap';
 import { BarChart } from 'react-native-gifted-charts';
+import { AntDesign } from "@expo/vector-icons"
 
 const Compare = () => {
+    const { theme } = useThemeContext()
     const { dex } = useDexContext()
     const [ selectedPokemon, setSelectedPokemon ] = useState([])
     const options = dex.flatMap(mon => Object.values(mon.forms))
 
+    const themeSetting1 = theme.mode === "dark" ? "#fff" : "#000"
+    const themeSetting2 = theme.mode === "dark" ? "#0c0c0c" : "#fff"
+
     return (
-        <SafeAreaView style={{flex: 1}}>
+        <SafeAreaView style={{flex: 1, backgroundColor: themeSetting2}}>
             <View style={{marginHorizontal: 5}}>
-                <Text style={styles.titleText}>Compare Pokemon</Text>
+                <Text style={[styles.titleText, {color: themeSetting1}]}>Compare Pokemon</Text>
                 <Dropdown
                     isSearchable
                     placeholder='Select Pokemon to Compare'
                     disabled={selectedPokemon.length >= 8}
                     options={options.map(item => {return{value: item, label: formatName(item.name)}})}
+                    dropdownStyle={{borderColor: themeSetting1, backgroundColor: themeSetting2}}
+                    selectedItemStyle={{color: themeSetting1}}
+                    placeholderStyle={{color: themeSetting1}}
+                    dropdownIcon={<AntDesign name="down" size={20} color={themeSetting1} />}
+                    dropdownIconStyle={{top: 20}}
+                    modalControls={{
+                        modalOptionsContainerStyle: {
+                            backgroundColor: themeSetting2
+                        }
+                    }}
+                    checkboxControls={{
+                        checkboxLabelStyle: {
+                            color: themeSetting1
+                        },
+                        checkboxStyle: {
+                            borderColor: themeSetting1
+                        },
+                    }}
+                    searchControls={{
+                        textInputStyle :{
+                            color: themeSetting1,
+                            backgroundColor: themeSetting2,
+                            borderColor: themeSetting1
+                        }
+                    }}
                     onValueChange={value => {
                         if (!selectedPokemon.includes(value)) {
                             setSelectedPokemon(prev => [...prev, value])
@@ -32,16 +63,16 @@ const Compare = () => {
                     <View>
                         {
                             selectedPokemon.length > 0 &&
-                            <View style={{borderRightWidth: 1}}>
-                                <View style={{borderBottomWidth: 1, height: 205}} />
-                                <View style={{height: 70, justifyContent: 'center', borderBottomWidth: 1}}>
-                                    <Text style={styles.headerText}>Abilities</Text>
+                            <View style={{borderRightWidth: 1, borderColor: themeSetting1}}>
+                                <View style={{borderBottomWidth: 1, height: 205, borderColor: themeSetting1}} />
+                                <View style={{height: 70, justifyContent: 'center', borderBottomWidth: 1, borderColor: themeSetting1}}>
+                                    <Text style={[styles.headerText, {color: themeSetting1}]}>Abilities</Text>
                                 </View>
-                                <View style={{height: 240, justifyContent: 'center', borderBottomWidth: 1}}>
-                                    <Text style={styles.headerText}>Base Stats</Text>
+                                <View style={{height: 240, justifyContent: 'center', borderBottomWidth: 1, borderColor: themeSetting1}}>
+                                    <Text style={[styles.headerText, {color: themeSetting1}]}>Base Stats</Text>
                                 </View>
-                                <View style={{height: 400, justifyContent: 'center', borderBottomWidth: 1}}>
-                                    <Text style={styles.headerText}>Types</Text>
+                                <View style={{height: 400, justifyContent: 'center'}}>
+                                    <Text style={[styles.headerText, {color: themeSetting1}]}>Types</Text>
                                 </View>
                             </View>            
                         }
@@ -60,8 +91,8 @@ const Compare = () => {
                                 const typeRelations = getTypeMatchups(mon.types)
 
                                 return (
-                                    <View style={{borderRightWidth: 1, width: 240}}>
-                                        <View style={{paddingHorizontal: 4, alignItems: "center", height: 205, borderBottomWidth: 1}}>
+                                    <View style={{borderRightWidth: 1, width: 240, borderColor: themeSetting1}}>
+                                        <View style={{paddingHorizontal: 4, alignItems: "center", height: 205, borderBottomWidth: 1, borderColor: themeSetting1}}>
                                             <TouchableOpacity 
                                                 style={styles.button} 
                                                 onPress={() => {
@@ -74,7 +105,7 @@ const Compare = () => {
                                                 source={{uri: mon.image}}
                                                 style={{width: 100, height: 100}}
                                             />
-                                            <Text style={styles.headerText}>{formatName(mon.name)}</Text>
+                                            <Text style={[styles.headerText, {color: themeSetting1}]}>{formatName(mon.name)}</Text>
                                             <View>
                                                 <View style={[styles.typeContainter, {backgroundColor: typeToColourMap[mon.types[0]]}]}>
                                                     <IconTypeMapper type={mon.types[0]} width={32} height={32} fill="white"/>
@@ -94,12 +125,12 @@ const Compare = () => {
                                                 }
                                             </View>
                                         </View>
-                                        <View style={{alignItems: "center", justifyContent: 'center', borderBottomWidth: 1, height: 70}}>
+                                        <View style={{alignItems: "center", justifyContent: 'center', borderBottomWidth: 1, height: 70, borderColor: themeSetting1}}>
                                             {
                                                 mon.abilities.map((ability, index) => {
                                                     return (
                                                         <View style={{flexDirection: "row"}} key={index}>
-                                                            <Text style={styles.defaultText}>{formatText(ability.name)}</Text>
+                                                            <Text style={[styles.defaultText, {color: themeSetting1}]}>{formatText(ability.name)}</Text>
                                                             {
                                                                 ability.hidden && 
                                                                 <View style={styles.hiddenIndicator}>
@@ -111,7 +142,7 @@ const Compare = () => {
                                                 })
                                             }
                                         </View>
-                                        <View style={{borderBottomWidth: 1, height: 240}}>
+                                        <View style={{borderBottomWidth: 1, height: 240, borderColor: themeSetting1}}>
                                             <BarChart
                                                 data={statData}
                                                 initialSpacing={0}
@@ -126,8 +157,8 @@ const Compare = () => {
                                                 yAxisThickness={0}
                                                 xAxisThickness={0}
                                                 showValuesAsTopLabel={true}
-                                                xAxisLabelTextStyle={styles.defaultText}
-                                                topLabelTextStyle={styles.defaultText}
+                                                xAxisLabelTextStyle={[styles.defaultText, {color: themeSetting1}]}
+                                                topLabelTextStyle={[styles.defaultText, {color: themeSetting1}]}
                                             />
                                         </View>
                                         <View>
@@ -135,7 +166,7 @@ const Compare = () => {
                                                 {
                                                     typeRelations.doubleWeakness.length > 0 &&
                                                     <View>
-                                                        <Text style={[styles.typeRelationBox, {borderColor: "#eb5545"}]}>x4</Text>
+                                                        <Text style={[styles.typeRelationBox, {borderColor: "#eb5545", color: themeSetting1}]}>x4</Text>
                                                         <View style={{flexDirection: "row", marginBottom: 5, flexWrap: "wrap"}}>
                                                         {
                                                             typeRelations.doubleWeakness.map((item, index) => {
@@ -152,7 +183,7 @@ const Compare = () => {
                                                 {
                                                     typeRelations.weaknesses.length > 0 &&
                                                     <View>
-                                                        <Text style={[styles.typeRelationBox, {borderColor: "#eb5545"}]}>x2</Text>
+                                                        <Text style={[styles.typeRelationBox, {borderColor: "#eb5545", color: themeSetting1}]}>x2</Text>
                                                         <View style={{flexDirection: "row", marginBottom: 5, flexWrap: "wrap"}}>
                                                         {
                                                             typeRelations.weaknesses.map((item, index) => {
@@ -170,7 +201,7 @@ const Compare = () => {
                                             {
                                                 typeRelations.resistances.length > 0 &&
                                                 <View>
-                                                    <Text style={[styles.typeRelationBox, {borderColor: "#68c367"}]}>x1/2</Text>
+                                                    <Text style={[styles.typeRelationBox, {borderColor: "#68c367", color: themeSetting1}]}>x1/2</Text>
                                                     <View style={{flexDirection: "row", flexWrap: "wrap", flexWrap: "wrap"}}>
                                                     {
                                                         typeRelations.resistances.map((item, index) => {
@@ -187,7 +218,7 @@ const Compare = () => {
                                             {
                                                 typeRelations.doubleResistances.length > 0 &&
                                                 <View>
-                                                    <Text style={[styles.typeRelationBox, {borderColor: "#68c367"}]}>x1/4</Text>
+                                                    <Text style={[styles.typeRelationBox, {borderColor: "#68c367", color: themeSetting1}]}>x1/4</Text>
                                                     <View style={{flexDirection: "row", marginBottom: 5}}>
                                                     {
                                                         typeRelations.doubleResistances.map((item, index) => {
@@ -204,7 +235,7 @@ const Compare = () => {
                                             {
                                                 typeRelations.immunities.length > 0 &&
                                                 <View>
-                                                    <Text style={[styles.typeRelationBox, {borderColor: "#68c367"}]}>x0</Text>
+                                                    <Text style={[styles.typeRelationBox, {borderColor: "#68c367", color: themeSetting1}]}>x0</Text>
                                                     <View style={{flexDirection: "row"}}>
                                                     {
                                                         typeRelations.immunities.map((item, index) => {
