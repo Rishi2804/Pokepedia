9c098073d746e45b0424ab7f504f7bd8d4b19db1
Rishi2804 <77213736+Rishi2804@users.noreply.github.com>
2024-05-06 21:25:31 -0400

    feat: added theme to settings page

--- app/Settings.js
+++ app/Settings.js
@@ -5,29 +5,32 @@ import { TouchableOpacity } from "react-native-gesture-handler";
  const Settings = () => {
 
     const { theme, updateTheme } = useThemeContext()
+    const themeSettings1 = theme.mode === "dark" ? "#000" : "#f2f2f7"
+    const themeSettings2 = theme.mode === "dark" ? "#1c1c1e" : "#fff"
+    const themeSetting3 = theme.mode === "dark" ? "#fff" : "#000"
 
     return (
-        <SafeAreaView style={styles.background}>
+        <SafeAreaView style={{backgroundColor: themeSettings1, flex: 1}}>
             <Text style={styles.settingHeaderText}>Theme Settings</Text>
-            <View style={styles.settingContainer}>
+            <View style={[styles.settingContainer, {backgroundColor: themeSettings2}]}>
                 <TouchableOpacity onPress={() => {updateTheme({mode: "light"})}}>
                     <View style={styles.settingSection}>
-                        <Text>Light Mode</Text>
-                        <View style={[styles.radioButton, {backgroundColor: "#b0d85d"}]}/>
+                        <Text style={[styles.settingText, {color: themeSetting3}]}>Light Mode</Text>
+                        <View style={[styles.radioButton, {backgroundColor: (theme.mode === "light" && !theme.system)  ? "#b0d85d" : themeSettings2}]}/>
                     </View>
                 </TouchableOpacity>
                 <TouchableOpacity onPress={() => {updateTheme({mode: "dark"})}}>
                 <View style={{height: 1, backgroundColor: "#b0d85d"}}/>
                     <View style={styles.settingSection}>
-                        <Text>Dark Mode</Text>
-                        <View style={[styles.radioButton, {backgroundColor: "#b0d85d"}]}/>
+                        <Text style={[styles.settingText, {color: themeSetting3}]}>Dark Mode</Text>
+                        <View style={[styles.radioButton, {backgroundColor: (theme.mode === "dark" && !theme.system)  ? "#b0d85d" : themeSettings2}]}/>
                     </View>
                 </TouchableOpacity>
                 <TouchableOpacity onPress={() => {updateTheme({system: true})}}>
                 <View style={{height: 1, backgroundColor: "#b0d85d"}}/>
                     <View style={styles.settingSection}>
-                        <Text>System</Text>
-                        <View style={[styles.radioButton, {backgroundColor: "#b0d85d"}]}/>
+                        <Text style={[styles.settingText, {color: themeSetting3}]}>System</Text>
+                        <View style={[styles.radioButton, {backgroundColor: theme.system ? "#b0d85d" : themeSettings2}]}/>
                     </View>
                 </TouchableOpacity>
             </View>
@@ -38,15 +41,13 @@ import { TouchableOpacity } from "react-native-gesture-handler";
  export default Settings;
 
  const styles = StyleSheet.create({
-    background: {
-        backgroundColor: "#f2f2f7"
-    },
     settingHeaderText: {
         fontFamily: "Geologica SemiBold",
         color: "#b0d85d",
         fontSize: 20,
         paddingTop: 20,
-        paddingBottom: 10
+        paddingBottom: 10,
+        marginLeft: 10
     },
     settingContainer: {
         backgroundColor: "#fff",
@@ -67,5 +68,10 @@ import { TouchableOpacity } from "react-native-gesture-handler";
         borderWidth: 2,
         borderColor: "#b0d85d",
         marginRight: 10
+    },
+    settingText: {
+        fontFamily: "Inconsolata Regular",
+        fontSize: 17,
+        paddingHorizontal: 5
     }
  })
